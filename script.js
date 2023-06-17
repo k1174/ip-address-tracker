d4f712e2df6081e856dfd69def90ad0841c4f03b
kamlesh <k24suthar@gmail.com>
2023-06-18 00:48:45 +0530

    V2.0. lots of update, Now can see viewers ip

--- script.js
+++ script.js
@@ -1,70 +1,58 @@
-
-let lat = 0
-let lng = 0
-
-
-
-//loading api request
-async function loadApi() {
-    let ip = search.value
-    return (
-        await fetch(
-            `https://geo.ipify.org/api/v2/country,city?apiKey=at_OI2BK3MLwyGUD2MOyNBwqrNTyjIWr&ipAddress=${ip}`
-        )
-    ).json();
-}
-
-// getting ip
+// html from data
 const submit = document.querySelector(".submit")
 const search = document.getElementById("searchBar")
 
+// updating variable
+const ipVal = document.querySelector(".ip")
+const locVal = document.querySelector(".loc")
+const timeVal = document.querySelector(".time")
+const ispVal = document.querySelector(".isp")
+
+let data
 
 submit.addEventListener("click", async (e) => {
-    let data 
-
-    const ipVal = document.querySelector(".ip")
-    const locVal = document.querySelector(".loc")
-    const timeVal = document.querySelector(".time")
-    const ispVal = document.querySelector(".isp")
 
     e.preventDefault()
 
     try {
-
         if (!search.value) {
-            
             throw new TypeError("Search input cannot be empty");
         }
+
         else {
-
-            //api request
-            data = await loadApi()//wait karege jub tak answer nhi mil jata
-
-            // display
-            ipVal.innerHTML = data.ip
-            locVal.innerHTML = data.location.country + " " + data.location.city
-            timeVal.innerHTML = data.location.timezone
-            ispVal.innerHTML = data.isp
-
-            //setting map
-            lat = parseFloat(data.location.lat)
-            lng = parseFloat(data.location.lng)
-
-            //marker pointer
-            let marker = L.marker([lat, lng]).addTo(map)
-                .bindPopup('<b>Hello world!</b><br />I am a popup.').openPopup();
-            map.flyTo([lat, lng], 13);
+            fetch(`https://ipapi.co/${search.value}/json/`)
+                .then(res => res.json())
+                .then(data => renderResults(data))
+                .catch(error => displayError(error));
 
             //clearing 
             search.value = ''
         }
-
-
     } catch (e) {
         console.log(e)
     }
-
 })
+
+// for viewers ip
+fetch('https://ipapi.co/json/')
+    .then(res => res.json())
+    .then(data => renderResults(data))
+    .catch(error => displayError(error));
+
+// for upadting page
+function renderResults(data) {
+
+    //info update
+    ipVal.innerHTML = data.ip
+    locVal.innerHTML = `${data.city}, ${data.region}, ${data.country_name}`
+    timeVal.innerHTML = data.timezone
+    ispVal.innerHTML = data.org
+
+    // map update
+    let marker = L.marker([data.latitude, data.longitude]).addTo(map)
+        .bindPopup('<b>Hello world!</b><br />I am a popup.').openPopup();
+    map.flyTo([data.latitude, data.longitude], 13);
+}
 
 //basic setup for map
 let map = L.map('map').setView([lat, lng], 13);
@@ -72,36 +60,3 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-
-
-
-
-
-
-
-
-// function getIP() {
-//     const ip = document.getElementById("searchBar").value;
-//     alert(typeof(ip));
-//     const url = "https://geo.ipify.org/api/v2/country,city?apiKey=at_8qm81JZtUM8YIeG6kawnualbmRpIS&ipAddress=" + ip;
-
-//     // Create the XMLHttpRequest object
-//     let request = new XMLHttpRequest();
-//     request.open("GET", url);
-//     request.send();
-
-//     request.onload = () => {
-//         if (request.status >= 200 && request.status < 400) {
-//             console.log(request.responseText);
-//         } else {
-//             alert("onload");
-//             console.log("Request returned an error status.");
-//         }
-//     };
-
-//     request.onerror = () => {
-//         console.log("Error occurred during the request.");
-//     };
-// }
-
-
