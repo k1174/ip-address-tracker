1d9eade742c67eb8399dffef784c4cb96bc85021
kamlesh <k24suthar@gmail.com>
2024-08-09 22:45:37 +0530

    minor changes

--- script.js
+++ script.js
@@ -8,8 +8,6 @@ const locVal = document.querySelector(".loc")
 const timeVal = document.querySelector(".time")
 const ispVal = document.querySelector(".isp")
 
-let data
-
 submit.addEventListener("click", async (e) => {
 
     e.preventDefault()
@@ -20,10 +18,10 @@ submit.addEventListener("click", async (e) => {
         }
 
         else {
-            fetch(`https://ipapi.co/${search.value}/json/`)
+            await fetch(`https://ipapi.co/${search.value}/json/`)
                 .then(res => res.json())
                 .then(data => renderResults(data))
-                .catch(error => displayError(error));
+                .catch(error => (error));
 
             //clearing 
             search.value = ''
@@ -37,7 +35,7 @@ submit.addEventListener("click", async (e) => {
 fetch('https://ipapi.co/json/')
     .then(res => res.json())
     .then(data => renderResults(data))
-    .catch(error => displayError(error));
+    .catch(error => (error));
 
 // for upadting page
 function renderResults(data) {
@@ -47,15 +45,15 @@ function renderResults(data) {
     locVal.innerHTML = `${data.city}, ${data.region}, ${data.country_name}`
     timeVal.innerHTML = data.timezone
     ispVal.innerHTML = data.org
-
+    console.log("update")
     // map update
     let marker = L.marker([data.latitude, data.longitude]).addTo(map)
-        .bindPopup('<b>Hello world!</b><br />I am a popup.').openPopup();
+        .bindPopup(`<b>Hello!</b><br/> WelCome to ${data.city}.`).openPopup();
     map.flyTo([data.latitude, data.longitude], 13);
 }
 
 //basic setup for map
-let map = L.map('map').setView([lat, lng], 13);
+let map = L.map('map').setView([19.0748, 72.8856], 13);
 const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
